af1fb4f0566dc9c0c8268ff4727dd9ab0efb4703
Andrey Svyrskyi <akkad1v@gmail.com>
2024-01-20 00:21:27 +0200

    done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -9,9 +9,10 @@ messageInput.value = savedDataLS.message || '';
 
 form.addEventListener('input', function (e) {
   if (e.target === emailInput || e.target === messageInput) {
+    localStorage.removeItem('feedback-form-state');
     const currentDataLS = {
-      email: emailInput.value,
-      message: messageInput.value,
+      email: emailInput.value.trim(),
+      message: messageInput.value.trim(),
     };
     localStorage.setItem('feedback-form-state', JSON.stringify(currentDataLS));
   }
@@ -19,14 +20,18 @@ form.addEventListener('input', function (e) {
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
+  if (emailInput.value && messageInput.value) {
+    const formData = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
 
-  const formData = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  console.log(formData);
+    console.log(formData);
 
-  localStorage.removeItem('feedback-form-state');
-  emailInput.value = '';
-  messageInput.value = '';
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+  } else {
+    alert('Please fill in both form fields');
+  }
 });
